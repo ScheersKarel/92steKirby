f5d7121dceef66c510ae7803008f81eca1371a83
Karel Scheers <karel@tbd.be>
2025-04-01 22:37:46 +0200

    debug true

--- site/config/config.php
+++ site/config/config.php
@@ -1,23 +1,24 @@
 <?php
 
 return [
-  'uploads' => [
-    'size' => 1000, // Maximale bestandsgrootte in MB
-    'types' => ['jpg', 'jpeg', 'png', 'gif', 'webp'], // Toegestane bestandstypes
-  ],
-  'media' => [
-    'upload' => [
-      'maxSize' => '1000M', // Maximale bestandsgrootte
+    'debug' => true,
+    'uploads' => [
+        'size' => 1000, // Maximale bestandsgrootte in MB
+        'types' => ['jpg', 'jpeg', 'png', 'gif', 'webp'], // Toegestane bestandstypes
     ],
-  ],
-  // Andere configuraties
-  'media' => [
-    'upload' => [
-      'maxSize' => '1000M', // Maximale bestandsgrootte in megabytes
-      'types' => ['jpg', 'jpeg', 'png', 'gif', 'webp'], // Toegestane bestandstypes
+    'media' => [
+        'upload' => [
+            'maxSize' => '1000M', // Maximale bestandsgrootte
+        ],
     ],
-  ],
-  'panel' =>[
-    'install' => true
-  ],
+    // Andere configuraties
+    'media' => [
+        'upload' => [
+            'maxSize' => '1000M', // Maximale bestandsgrootte in megabytes
+            'types' => ['jpg', 'jpeg', 'png', 'gif', 'webp'], // Toegestane bestandstypes
+        ],
+    ],
+    'panel' => [
+        'install' => true
+    ],
 ];
